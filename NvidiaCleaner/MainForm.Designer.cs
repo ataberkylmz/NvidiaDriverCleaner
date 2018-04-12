9a929d3e8eac162c1c0e413bdc242599399db715
Ata Berk YILMAZ <ataberkylmz@gmail.com>
2018-04-12 14:06:24 +0300

    Add project files.

---
+++ NvidiaCleaner/MainForm.Designer.cs
@@ -0,0 +1,266 @@
+﻿namespace NvidiaCleaner
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabMain = new System.Windows.Forms.TabPage();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.freeUpSpaceButton = new System.Windows.Forms.Button();
+            this.progressBar = new System.Windows.Forms.ProgressBar();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textArea = new System.Windows.Forms.RichTextBox();
+            this.tabSettings = new System.Windows.Forms.TabPage();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.cGenerateLog = new System.Windows.Forms.CheckBox();
+            this.richTextBox4 = new System.Windows.Forms.RichTextBox();
+            this.cWindowsDriver = new System.Windows.Forms.CheckBox();
+            this.basicSettingsGroup = new System.Windows.Forms.GroupBox();
+            this.richTextBox3 = new System.Windows.Forms.RichTextBox();
+            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.cRepository = new System.Windows.Forms.CheckBox();
+            this.cDownloaded = new System.Windows.Forms.CheckBox();
+            this.cExtracted = new System.Windows.Forms.CheckBox();
+            this.tabControl1.SuspendLayout();
+            this.tabMain.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.tabSettings.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.basicSettingsGroup.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabMain);
+            this.tabControl1.Controls.Add(this.tabSettings);
+            resources.ApplyResources(this.tabControl1, "tabControl1");
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            // 
+            // tabMain
+            // 
+            this.tabMain.Controls.Add(this.groupBox2);
+            this.tabMain.Controls.Add(this.groupBox1);
+            resources.ApplyResources(this.tabMain, "tabMain");
+            this.tabMain.Name = "tabMain";
+            this.tabMain.UseVisualStyleBackColor = true;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.freeUpSpaceButton);
+            this.groupBox2.Controls.Add(this.progressBar);
+            resources.ApplyResources(this.groupBox2, "groupBox2");
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.TabStop = false;
+            // 
+            // freeUpSpaceButton
+            // 
+            resources.ApplyResources(this.freeUpSpaceButton, "freeUpSpaceButton");
+            this.freeUpSpaceButton.Name = "freeUpSpaceButton";
+            this.freeUpSpaceButton.UseVisualStyleBackColor = true;
+            this.freeUpSpaceButton.Click += new System.EventHandler(this.FreeUpSpaceButton_Click);
+            // 
+            // progressBar
+            // 
+            resources.ApplyResources(this.progressBar, "progressBar");
+            this.progressBar.Name = "progressBar";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.textArea);
+            resources.ApplyResources(this.groupBox1, "groupBox1");
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            resources.ApplyResources(this.label1, "label1");
+            this.label1.Name = "label1";
+            // 
+            // textArea
+            // 
+            this.textArea.BackColor = System.Drawing.Color.White;
+            this.textArea.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textArea.Cursor = System.Windows.Forms.Cursors.Default;
+            resources.ApplyResources(this.textArea, "textArea");
+            this.textArea.Name = "textArea";
+            this.textArea.ReadOnly = true;
+            this.textArea.TabStop = false;
+            // 
+            // tabSettings
+            // 
+            this.tabSettings.Controls.Add(this.groupBox3);
+            this.tabSettings.Controls.Add(this.basicSettingsGroup);
+            resources.ApplyResources(this.tabSettings, "tabSettings");
+            this.tabSettings.Name = "tabSettings";
+            this.tabSettings.UseVisualStyleBackColor = true;
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.cGenerateLog);
+            this.groupBox3.Controls.Add(this.richTextBox4);
+            this.groupBox3.Controls.Add(this.cWindowsDriver);
+            this.groupBox3.ForeColor = System.Drawing.Color.Maroon;
+            resources.ApplyResources(this.groupBox3, "groupBox3");
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.TabStop = false;
+            // 
+            // cGenerateLog
+            // 
+            resources.ApplyResources(this.cGenerateLog, "cGenerateLog");
+            this.cGenerateLog.ForeColor = System.Drawing.Color.Maroon;
+            this.cGenerateLog.Name = "cGenerateLog";
+            this.cGenerateLog.UseVisualStyleBackColor = true;
+            // 
+            // richTextBox4
+            // 
+            this.richTextBox4.BackColor = System.Drawing.Color.White;
+            this.richTextBox4.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.richTextBox4.ForeColor = System.Drawing.Color.Maroon;
+            resources.ApplyResources(this.richTextBox4, "richTextBox4");
+            this.richTextBox4.Name = "richTextBox4";
+            this.richTextBox4.ReadOnly = true;
+            this.richTextBox4.TabStop = false;
+            // 
+            // cWindowsDriver
+            // 
+            resources.ApplyResources(this.cWindowsDriver, "cWindowsDriver");
+            this.cWindowsDriver.ForeColor = System.Drawing.Color.Maroon;
+            this.cWindowsDriver.Name = "cWindowsDriver";
+            this.cWindowsDriver.UseVisualStyleBackColor = true;
+            this.cWindowsDriver.CheckedChanged += new System.EventHandler(this.CWindowsDriver_CheckedChanged);
+            // 
+            // basicSettingsGroup
+            // 
+            this.basicSettingsGroup.Controls.Add(this.richTextBox3);
+            this.basicSettingsGroup.Controls.Add(this.richTextBox2);
+            this.basicSettingsGroup.Controls.Add(this.richTextBox1);
+            this.basicSettingsGroup.Controls.Add(this.cRepository);
+            this.basicSettingsGroup.Controls.Add(this.cDownloaded);
+            this.basicSettingsGroup.Controls.Add(this.cExtracted);
+            resources.ApplyResources(this.basicSettingsGroup, "basicSettingsGroup");
+            this.basicSettingsGroup.Name = "basicSettingsGroup";
+            this.basicSettingsGroup.TabStop = false;
+            // 
+            // richTextBox3
+            // 
+            this.richTextBox3.BackColor = System.Drawing.Color.White;
+            this.richTextBox3.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            resources.ApplyResources(this.richTextBox3, "richTextBox3");
+            this.richTextBox3.Name = "richTextBox3";
+            this.richTextBox3.ReadOnly = true;
+            this.richTextBox3.TabStop = false;
+            // 
+            // richTextBox2
+            // 
+            this.richTextBox2.BackColor = System.Drawing.Color.White;
+            this.richTextBox2.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            resources.ApplyResources(this.richTextBox2, "richTextBox2");
+            this.richTextBox2.Name = "richTextBox2";
+            this.richTextBox2.ReadOnly = true;
+            this.richTextBox2.TabStop = false;
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.BackColor = System.Drawing.Color.White;
+            this.richTextBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            resources.ApplyResources(this.richTextBox1, "richTextBox1");
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.ReadOnly = true;
+            this.richTextBox1.TabStop = false;
+            // 
+            // cRepository
+            // 
+            resources.ApplyResources(this.cRepository, "cRepository");
+            this.cRepository.Name = "cRepository";
+            this.cRepository.UseVisualStyleBackColor = true;
+            // 
+            // cDownloaded
+            // 
+            resources.ApplyResources(this.cDownloaded, "cDownloaded");
+            this.cDownloaded.Name = "cDownloaded";
+            this.cDownloaded.UseVisualStyleBackColor = true;
+            // 
+            // cExtracted
+            // 
+            resources.ApplyResources(this.cExtracted, "cExtracted");
+            this.cExtracted.Name = "cExtracted";
+            this.cExtracted.UseVisualStyleBackColor = true;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            resources.ApplyResources(this, "$this");
+            this.Controls.Add(this.tabControl1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "MainForm";
+            this.tabControl1.ResumeLayout(false);
+            this.tabMain.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.tabSettings.ResumeLayout(false);
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            this.basicSettingsGroup.ResumeLayout(false);
+            this.basicSettingsGroup.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabMain;
+        private System.Windows.Forms.TabPage tabSettings;
+        private System.Windows.Forms.ProgressBar progressBar;
+        private System.Windows.Forms.Button freeUpSpaceButton;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.RichTextBox textArea;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.RichTextBox richTextBox4;
+        private System.Windows.Forms.CheckBox cWindowsDriver;
+        private System.Windows.Forms.GroupBox basicSettingsGroup;
+        private System.Windows.Forms.RichTextBox richTextBox3;
+        private System.Windows.Forms.RichTextBox richTextBox2;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.CheckBox cRepository;
+        private System.Windows.Forms.CheckBox cDownloaded;
+        private System.Windows.Forms.CheckBox cExtracted;
+        private System.Windows.Forms.CheckBox cGenerateLog;
+    }
+}
+
---
+++ NvidiaCleaner/MainForm.cs
@@ -0,0 +1,314 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.Win32;
+using System.Threading;
+using System.Security.Principal;
+using NvidiaCleaner.Lib;
+
+
+namespace NvidiaCleaner
+{
+    public partial class MainForm : Form
+    {
+        private const string logFileName = "NCleaner.log";
+        //Get System path.
+        private readonly string _osPath = Path.GetPathRoot(Environment.SystemDirectory);
+        private readonly List<string> _errors = new List<string>();
+        private double totalCleanedMb=0;
+
+        public MainForm()
+        {
+            InitializeComponent();
+            //Initial Settings.
+            cExtracted.Checked = true;
+            cDownloaded.Checked = true;
+        }
+        
+        //Calculates the percentage of each checked setting except generate log.
+        public int GetBarPercentage()
+        {
+            int total=0;
+            int result=0;
+
+            if (cDownloaded.Checked == true)
+                total++;
+            if (cExtracted.Checked == true)
+                total++;
+            if (cRepository.Checked == true)
+                total++;
+            if (cWindowsDriver.Checked == true)
+                total++;
+
+            switch (total)
+            {
+                case 1:
+                    result = 100;
+                    break;
+                case 2:
+                    result = 50;
+                    break;
+                case 3:
+                    result = 33;
+                    progressBar.Maximum = 99;
+                    break;
+                case 4:
+                    result = 25;
+                    break;
+                default:
+                    break;
+            }
+            return result;
+        }
+
+        //Method that delets the extracted drivers at System:\NVIDIA path.
+        public void DeleteExtractedDrivers()
+        {
+            string path = _osPath + @"NVIDIA";
+            double deletedTotal = 0;
+            textArea.Text += @"-----Extracted Drivers-----" + Environment.NewLine;
+
+            if (Directory.Exists(path))
+            {
+                //MessageBox.Show(_path + @" Exist!");
+                textArea.Text += path + @" FOUND! " + Environment.NewLine;
+
+                long sizeB = NVC.GetDirectorySize(path);
+                double sizeMB = NVC.SizeConvert(sizeB, "MB");
+
+                try
+                {
+                    Directory.Delete(path, true);
+                    textArea.Text += path + @" Deleted! " + sizeMB + @" MB cleaned!" + Environment.NewLine;
+                    deletedTotal += sizeMB;
+                    progressBar.Value += GetBarPercentage();
+                }
+                catch (Exception e)
+                {
+                    _errors.Add(e.ToString());
+                    textArea.Text += path + @" is not deleted! " + Environment.NewLine + @"Error: " + e.Message + Environment.NewLine;
+                }  
+            }
+            else
+            {
+                textArea.Text += path + @" NOT FOUND! PASSING... " + Environment.NewLine;
+                progressBar.Value += GetBarPercentage();
+            }
+
+            textArea.Text += @"Total " + deletedTotal + @" MB Deleted in " + path + Environment.NewLine;
+            textArea.Text += @"-----Extracted Drivers End-----" + Environment.NewLine + Environment.NewLine;
+            totalCleanedMb += deletedTotal;
+        }
+
+
+        //Method that delets the downloaded drivers at System:\ProgramData\NVIDIA Corporation\Downloader path.
+        public void DeleteDownloadedDrivers()
+        {
+            string path = _osPath + @"ProgramData\NVIDIA Corporation\Downloader";
+            double deletedTotal = 0;
+            textArea.Text += @"-----Downloaded Drivers-----" + Environment.NewLine;
+
+            if (Directory.Exists(path))
+            {
+                //ONLY DELETE latest and random numbers.
+                string whiteList = "config"; //Maybe add latest but not necessary.
+                foreach (var dir in Directory.GetDirectories(path))
+                {
+                    if (!dir.Contains(whiteList))
+                    {
+                        textArea.Text += dir.Substring(45) + @" FOUND! " + Environment.NewLine;
+                        long sizeB = NVC.GetDirectorySize(dir);
+                        double sizeMB = NVC.SizeConvert(sizeB, "MB");
+
+                        try
+                        {
+                            Directory.Delete(dir, true);
+                            deletedTotal += sizeMB;
+                            textArea.Text += dir.Substring(45) + @" Deleted! " + sizeMB + @" MB cleaned!" + Environment.NewLine + Environment.NewLine;
+                        }
+                        catch (Exception e)
+                        {
+                            _errors.Add(e.ToString());
+                            textArea.Text += dir + @" is not deleted! " + Environment.NewLine + @"Error: " + e.Message + Environment.NewLine + Environment.NewLine;
+                        }
+                    }
+                }
+                progressBar.Value += GetBarPercentage();
+            }
+            else
+            {
+                textArea.Text += path + @" NOT FOUND! PASSING... " + Environment.NewLine;
+                progressBar.Value += GetBarPercentage();
+            }
+            textArea.Text += @"Total " + deletedTotal + @" MB Deleted in " + path + Environment.NewLine;
+            textArea.Text += @"-----Downloaded Drivers End-----" + Environment.NewLine + Environment.NewLine;
+            totalCleanedMb += deletedTotal;
+        }
+
+        //Method that delets the downloaded drivers at System:\Program Files\NVIDIA Corporation\Installer2 path.
+        public void DeleteRepository()
+        {
+            //DO NOT DELETE THE INSTALLER2 FOLDER ITSELF!!!
+            //Delete Repository.
+            string path = _osPath + @"Program Files\NVIDIA Corporation\Installer2";
+            double deletedTotal = 0;
+            textArea.Text += @"-----Repository-----" + Environment.NewLine;
+
+            if (Directory.Exists(path))
+            {
+                foreach (var dir in Directory.GetDirectories(path))
+                {
+                    textArea.Text += dir.Substring(47) + @" FOUND! " + Environment.NewLine;
+                    long sizeB = NVC.GetDirectorySize(dir);
+                    double sizeMB = NVC.SizeConvert(sizeB, "MB");
+
+                    try
+                    {
+                        Directory.Delete(dir, true);
+                        deletedTotal += sizeMB;
+                        textArea.Text += dir.Substring(47) + @" Deleted! " + sizeMB + @" MB cleaned!" + Environment.NewLine + Environment.NewLine;
+                    }
+                    catch (Exception e)
+                    {
+                        _errors.Add(e.ToString());
+                        textArea.Text += dir + @" is not deleted! " + Environment.NewLine + @"Error: " + e.Message + Environment.NewLine + Environment.NewLine;
+                    }
+                }
+                progressBar.Value += GetBarPercentage();
+            }
+            else
+            {
+                textArea.Text += path + @" NOT FOUND! PASSING... " + Environment.NewLine;
+                progressBar.Value += GetBarPercentage();
+            }
+            textArea.Text += @"Total " + deletedTotal + @" MB Deleted in " + path + Environment.NewLine;
+            textArea.Text += @"-----Repository End-----" + Environment.NewLine + Environment.NewLine;
+            totalCleanedMb += deletedTotal;
+        }
+
+        public void DeleteLogs()
+        {
+            string[] path ={ _osPath + @"ProgramData\NVIDIA Corporation\GeForce Experience\Logs", _osPath + @"ProgramData\NVIDIA Corporation\NVIDIA GeForce Experience\Logs"};
+
+            for (int i = 0; i < 2; i++)
+            {
+                double deletedTotal = 0;
+                textArea.Text += @"-----Unneccessary Logs "+(i+1)+@"-----" + Environment.NewLine;
+                if (Directory.Exists(path[i]))
+                {
+                    long sizeB = NVC.GetDirectorySize(path[i]);
+                    double sizeMB = NVC.SizeConvert(sizeB, "MB");
+
+                    foreach (var item in Directory.GetFiles(path[i]))
+                    {
+                        try
+                        {
+                            if (item.EndsWith(".log"))
+                            {
+                                File.Delete(item);
+                                textArea.Text += item + @" Deleted! " + Environment.NewLine;
+                            }
+                        }
+                        catch (Exception e)
+                        {
+                            _errors.Add(e.ToString());
+                            textArea.Text += item + @" is not deleted! " + Environment.NewLine + @"Error: " + e.Message + Environment.NewLine + Environment.NewLine;
+                        }
+                    }
+                    deletedTotal += sizeMB;
+                }
+                else
+                {
+                    textArea.Text += path[i] + @" NOT FOUND! PASSING... " + Environment.NewLine;
+                    progressBar.Value += GetBarPercentage();
+                }
+                textArea.Text += @"Total " + deletedTotal + @" MB Deleted in " + path[i] + Environment.NewLine;
+                textArea.Text += @"-----Unneccessary Logs "+(i+1)+@" End-----" + Environment.NewLine + Environment.NewLine;
+                totalCleanedMb += deletedTotal;
+            } 
+        }
+
+        public void DeleteWindowsDrivers()
+        {    
+            //TODO:Delete Repository using bruteforce. Use powershell script.
+        }
+
+        public void GenerateLog()
+        {
+            //TODO: Implement real log method.
+
+            string path = Environment.CurrentDirectory;
+
+            if (_errors.Count != 0)
+            {
+                try
+                {
+                    System.IO.File.WriteAllLines(path + @"\" + logFileName, _errors);
+                }
+                catch
+                {
+                    MessageBox.Show(@"Error while creating log.");
+                }
+            }
+
+            
+        }
+
+        private void FreeUpSpaceButton_Click(object sender, EventArgs e)
+        {
+            //Reset progress bar.
+            progressBar.Value = 0;
+            //Clear text area in order to write logs.
+            textArea.Clear();
+            //Disable editing settings.
+            tabSettings.Enabled = false;
+
+            GenerateLog();
+
+            try
+            {
+                if (cExtracted.Checked == true)
+                    DeleteExtractedDrivers();
+
+                if (cDownloaded.Checked == true)
+                {
+                    DeleteDownloadedDrivers();
+                    DeleteLogs();
+                }          
+
+                if (cRepository.Checked == true)
+                    DeleteRepository();
+
+                //if (cWindowsDriver.Checked == true)
+                    //DeleteWindowsDrivers();
+
+                //Done Message!
+                MessageBox.Show(@"Operation Successfull! " + totalCleanedMb + @" Mb Cleaned!", @"Success!");
+            }
+            catch (Exception ex)
+            {
+                _errors.Add(ex.Message);
+            }
+
+            //Enable editing settings upon finish.
+            tabSettings.Enabled = true;
+        }
+
+        private void CWindowsDriver_CheckedChanged(object sender, EventArgs e)
+        {
+            if (cWindowsDriver.Checked == true)
+            {
+                MessageBox.Show(@"This process may damage current installed driver if the driver is not running right now. Eg. You are in safe mode, etc.", @"Warning!!!");
+            }
+        }
+    }
+}
